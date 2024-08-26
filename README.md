46e97a07fdee3dfc18995db695f1ec090b80a00c
kanzuftw <farman1761md@gmail.com>
2024-08-26 07:24:27 +0530

    change

--- README.md
+++ README.md
@@ -14,6 +14,14 @@ Foundry consists of:
 https://book.getfoundry.sh/
 
 ## Usage
+1. Relative Stability: Anchored or Pegged -> $1.00
+   1. Chainlink Price feed.
+   2. Set a function to exchange ETH & BTC -> $$$$
+2. Stability Mechanism (Minting): Algorithm (Decentralized)
+   1. People can only ming the stablecoin with enough collateral (coded)
+3. Collateral: Exogenous (Crypto)
+   1. wETH
+   2. wBTC
 
 ### Build
 
---
+++ src/DSCEngine.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title DSCEngine
+ * @author Mohd Farman
+ *
+ * This system is designed to be as minimal as possible, and have the tokens maintain a 1 token == 1$ peg.
+ * The stablecoin has the properties:
+ * - Exogenous Collateral: ETH & BTC
+ * - Dollar pegged
+ * - Algorithmic Stable
+ *
+ * It is similar to DAI if DAI had no governance, no fees and only backed by WETH and WBTC.
+ *
+ * @notice The contract is the core of the DSC System. It handles all the logic of the mining and redeeming DSC, as well as depositing & withdrawing collateral.
+ * @notice The contract is very loosely based on the MakerDAO DSS (DAI) system, but with a few key differences.
+ */
+contract DSCEngine {
+
+}
+
